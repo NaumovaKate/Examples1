2e9deaf56d727dac7510920dae701f254cbe50f9
NaumovaKate <naumova.katrine@gmail.com>
2022-12-19 16:39:14 +0300

    снова сохранила

--- Example003_summ/Program.cs
+++ Example003_summ/Program.cs
@@ -1,5 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
-int numberA = 31;
-int numberB = 5;
-int result = numberA + numberB;
-Console.WriteLine(result);
+﻿int numberA = new Random().Next(1, 10); //1.2.3.4...9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA + numberB);
