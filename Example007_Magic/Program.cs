18c004275d09657b47df701ca7797ec99f7b2d2e
NaumovaKate <naumova.katrine@gmail.com>
2022-12-19 16:36:43 +0300

    сохранение

---
+++ Example007_Magic/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+        //Console.SetCursorPosition(10, 4);
+        //Console.WriteLine("+");
+
+        int xa = 1;
+        int ya = 10;
+        int xb = 6;
+        int yb = 30;
+        int xc = 60;
+        int yc = 30;
+
+        Console.SetCursorPosition(xa, ya);
+        Console.WriteLine("+");
+
+        Console.SetCursorPosition(xb, yb);
+        Console.WriteLine("+");
+
+        Console.SetCursorPosition(xc, yc);
+        Console.WriteLine("+");
+    }
+}
